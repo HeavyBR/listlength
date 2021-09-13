e782d8c306a5f4fa215b09c4b53466e5c611a314
Matheus Cumpian <matheus.cumpian@hotmail.com>
2021-09-13 19:37:45 -0300

    refactor: renaming a comment that has a typo

--- lib/listlength.ex
+++ lib/listlength.ex
@@ -5,6 +5,6 @@ defmodule Listlength do
   defp sum_items([], acc), do: acc
 
   defp sum_items([_ | tail], acc) do
-    sum_items(tail, acc + 1) # this accumulator is passed by params to force the compiler to use TCO (tail call optmization)
+    sum_items(tail, acc + 1) # this accumulator is passed by params to force the compiler to use TCO (tail call optimization)
   end
 end
